0bde6f24222ab549e4d6fdba33d4b842bae36a4c
Melia88 <castro.melissa8.com>
2021-05-10 13:35:11 -0600

    save

---
+++ neatpantry.client/src/pages/SheldDetailsPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="shelf-details-page">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShelfDetailsPage',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
