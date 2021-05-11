5150e23b52425cdfb7afc68ddbd607cd6242bff4
Melia88 <castro.melissa8.com>
2021-05-11 14:50:29 -0600

    save new

--- neatpantry.client/src/services/ShelvesService.js
+++ neatpantry.client/src/services/ShelvesService.js
@@ -5,6 +5,7 @@ import { api } from './AxiosService'
 class ShelvesService {
   async createShelf(body) {
     const res = await api.post('api/shelves', body)
+    logger.log(res.data)
     router.push({ name: 'ShelfDetailsPage', params: { id: res.data.id } })
     // this.getAllShelves()
   }
