e96be8f1a2d8bad895711b051d41950927a6496a
Michaela Bickish <mmbickish@gmail.com>
2021-05-13 12:11:52 -0600

    checkall boxes and item modal and shelf drop down

--- neatpantry.client/src/services/ShoppingListItemsService.js
+++ neatpantry.client/src/services/ShoppingListItemsService.js
@@ -1,6 +1,9 @@
 class ShoppingListItemsService {
-  async checkAll(items) {
-    document.querySelectorAll('input[type="checkbox"]')
+  async toggleCheck(items) {
+    document.getElementById('check-all-box').checked ? document.getElementById('check-one-box').checked = true : document.getElementById('check-one-box').checked = false
+    // $('#check-all-box').click(function() {
+    //   $('input[type=checkbox]').prop('checked', $(this).prop('checked'))
+    // })
   }
 }
 
